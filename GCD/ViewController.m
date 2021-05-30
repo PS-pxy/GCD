a3f3eb628e1a1e585a1f886271bc7d1ce8253645
PS_pxy <ps_panshen@163.com>
2021-05-30 23:02:50 +0800

    添加

--- GCD/ViewController.m
+++ GCD/ViewController.m
@@ -16,6 +16,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    //gcd
+    UIView *view = [UIView new];
+    [self.view addSubview:view];
 }
 
 
